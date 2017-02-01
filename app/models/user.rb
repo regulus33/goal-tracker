3bb0092b7bd46b6063591bf73f220cd1385f252e
regulus33 <zacharyrowden89@gmail.com>
2017-02-01 14:09:41 -0500

    Override authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,20 @@ class User < ApplicationRecord
 	# has_many :due_dates, through: :tasks
 	has_secure_password
 
-
-  def password=(password)
-    self.password_digest = BCrypt::Password.create(password)
-  end
+  def password
+  	self.password_digest
+  end 
 
   def is_password?(password)
-    BCrypt::Password.new(self.password_digest) == password
+    true
   end
+
+  # uncommment for working authentication and delete above method
+  # def password=(password)
+  #   self.password_digest = BCrypt::Password.create(password)
+  # end
+
+  # def is_password?(password)
+  #   BCrypt::Password.new(self.password_digest) == password
+  # end
 end
