a54cac1facc9089a769a05b6435429471df43f83
regulus33 <zacharyrowden89@gmail.com>
2017-02-01 19:57:16 -0500

    Write draft for task form.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,9 +4,22 @@ class TasksController < ApplicationController
 	end
 
 	def index 
-		render 'index'
-	end 
+	  render 'index'
+	end
 
+	def new
+	 @task = Task.new 
+	 @task.completions.build 
+	 @task.due_dates.build 
+	end 
+ 
 	def create 
-    end  
+
+    end 
+
+    private 
+
+	def session_params
+		params.require(:session).permit(:email, :password)
+	end 
 end
