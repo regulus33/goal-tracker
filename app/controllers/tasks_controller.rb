bee63193dc3dc74d2f2e6f56b426558633902709
regulus33 <zacharyrowden89@gmail.com>
2017-02-05 16:11:45 -0500

    Create valid datetime field in new task form.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,15 +7,20 @@ class TasksController < ApplicationController
 	 @task = Task.new 
 	 # @task.completions.build 
 	 @task.due_dates.build 
+	 @task.completions.build 
 	end 
  
 	def create 
 	  task = Task.new(name: task_params[:name])#create a task
-	  due_date = DueDate.new(date: task_params[:due_date][0][:date])#create a task
+	  due_date = DueDate.new(date: DateTime.parse(task_params[:due_date][0][:date]))#create a task
 	  task.due_dates << due_date#create a task
 	  current_user.tasks << task#create a task
-	  completion = Completion.new(completion: 0, completion_value: params[:completion_value])
-	  
+	  completion = Completion.new(completed: 0, completion_value: params[:completion_value].to_i)
+	  task.completions << completion
+	  due_date.save!
+	  completion.save!
+	  task.save!
+	  redirect_to '/tasks'
     end 
 
     private 
