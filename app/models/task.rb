292828176f551dab876842773d439eecfe656532
regulus33 <zacharyrowden89@gmail.com>
2017-02-15 20:13:27 -0500

    Make index partials only show incomplete tasks.

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,33 +9,37 @@ class Task < ApplicationRecord
 	has_many :due_dates
 
 	def due_today?
-		return true if self.due_dates.first.date.strftime('%F') == Time.now.strftime('%F')
+		return true if self.due_dates.first.date.strftime('%F') == Time.now.strftime('%F') && self.completions.last.completed == 0
 		false 
 	end
 
 	def due_this_week?
 		return true if due_today?
-		due_array = self.due_dates.first.date.strftime('%m/%d/%y').split("/")
-		today_array = Time.now.strftime('%m/%d/%y').split("/")
-		# if in the same month
-		if due_array[0] == today_array[0] && due_array[2] == today_array[2]
-			return true if due_array[1].to_i - today_array[1].to_i <= 7 
-		end
-		#if separate months 
-		if ( due_array[0]-1 ) == today_array[0] || ( due_array[0]+1 ) == today_array[0]
-			this_month_days = self.due_dates.first.days_in_month
-			todays_date_number = today_array[1].to_i 
-			due_date_number = due_array[1].to_i 
-			if (this_month_days - todays_date_number) + due_date_number <= 7
-				return true
+		if self.completions.last.completed == 0
+			due_array = self.due_dates.first.date.strftime('%m/%d/%y').split("/")
+			today_array = Time.now.strftime('%m/%d/%y').split("/")
+			# if in the same month
+			if due_array[0] == today_array[0] && due_array[2] == today_array[2]
+				return true if due_array[1].to_i - today_array[1].to_i <= 7 
 			end
-		end
+			#if separate months 
+			if ( due_array[0]-1 ) == today_array[0] || ( due_array[0]+1 ) == today_array[0]
+				this_month_days = self.due_dates.first.days_in_month
+				todays_date_number = today_array[1].to_i 
+				due_date_number = due_array[1].to_i 
+				if (this_month_days - todays_date_number) + due_date_number <= 7
+					return true
+				end
+			end
+		end 
 		false 
 	end
 
 	def generate_new_due_date
-		if been_one_second?
+		if been_5_seconds?
 			completion = Completion.new(completed: 0, completen_max: self.completions.last.completen_max)
+			self.completions << completion
+			completion.save!
 			# self.due_dates
 		end
 
@@ -50,8 +54,8 @@ class Task < ApplicationRecord
 	  # end
 	end
 
-	def been_one_second? #since last completion
-		return true if (DateTime.now.to_i - self.task.completions.completed_at.to_i) >= (1)
+	def been_5_seconds? #since last completion
+		return true if (DateTime.now.to_i - self.task.completions.completed_at.to_i) >= (5)
 		false 
     end 
 
