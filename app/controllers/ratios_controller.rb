a97996dd67ea425d98a5c4c297e218be3e28d99a
regulus33 <zacharyrowden89@gmail.com>
2017-02-27 12:58:01 -0500

    Add ratio save to ratios controller

--- app/controllers/ratios_controller.rb
+++ app/controllers/ratios_controller.rb
@@ -6,7 +6,9 @@ class RatiosController < ApplicationController
 	def save 
 		if current_user.due_dates.any?
 		 today = current_user.due_dates.last
-		 todays_ratio = current_user.task_completion_ratio_of_today
-		 
+		 todays_ratio = current_user.task_completion_ratio_of_today[1][:value] / current_user.task_completion_ratio_of_today[0][:value] 
+		 ratio = Ratio.new(value: todays_ratio)
+		 ratio.due_date = today 
+		 ratio.save!
 	end 
 end
