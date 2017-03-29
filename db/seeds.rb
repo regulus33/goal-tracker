b5a15b6a1e36f62f7ff0536d605820cc9d9b19e1
regulus33 <zacharyrowden89@gmail.com>
2017-03-28 21:20:59 -0400

    Write draft for new seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# completion_units = ["minutes", "reps", "boolean"]
-# terms = ["daily", "weekly", "monthly", "1 time"]
-20.times do |id| 
+completion_units = ["minutes", "reps", "boolean"]
+
+def last_thirty_days_array
+(1.month.ago.to_date..Date.today).map{ |date| date }
+end 
+
+terms = ["daily"]#, "weekly", "monthly", "1 time"]
+
+completions = [0,1]
+
+1.times do |user_id| 
 	User.create!(username: Faker::Name.name, email: Faker::Internet.free_email,  password_digest: "password")
-	Task.create!(user_id: (id + 1), name: (Faker::Hacker.verb + " " + Faker::Hacker.noun), completion_max: rand(1..100), completion_unit: completion_units.sample, term: terms.sample, description: Faker::ChuckNorris.fact)
-	DueDate.create!(task_id: (id + 1), date: Faker::Date.backward(rand(1..20)))
-	Completion.create!(task_id: (id + 1), completed: 0, completion_value: 0)
+	10.times do |task_id| 
+		# all completion max is 10 randomizing value on completions table 
+		Task.create!(user_id: (user_id + 1), name: (Faker::Hacker.verb + " " + Faker::Hacker.noun), completion_max: 10, completion_unit: completion_units.sample, term: terms.sample, description: Faker::ChuckNorris.fact) 
+		30.times do |date_id|
+			DueDate.create!(task_id: (task_id + 1), date: last_thirty_days_array[date_id])
+			Ratio.create!(due_date_id: (date_id +1), value: rand(0.01..1))
+			Completion.create!(task_id: (task_id + 1), completed: x = completions.sample, completion_value: x = 0 ? rand(1..9) : 10)
+		end
+	end 
 end
 
+# Faker::Date.backward(rand(1..20))
+
+    # create_table :ratios do |t|
+  	 #  t.integer :due_date_id 
+  	 #  t.float :value 
+     # t.timestamps
 
