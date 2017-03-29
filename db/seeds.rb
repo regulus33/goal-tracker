b9835cbedf45a6e49f1e971bb5c74137a867788b
regulus33 <zacharyrowden89@gmail.com>
2017-03-28 21:51:04 -0400

    Make working seeds, update 1 month ago seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,8 @@ completions = [0,1]
 		# all completion max is 10 randomizing value on completions table 
 		Task.create!(user_id: (user_id + 1), name: (Faker::Hacker.verb + " " + Faker::Hacker.noun), completion_max: 10, completion_unit: completion_units.sample, term: terms.sample, description: Faker::ChuckNorris.fact) 
 		30.times do |date_id|
-			DueDate.create!(task_id: (task_id + 1), date: last_thirty_days_array[date_id])
+			# binding.pry
+			DueDate.create!(task_id: (task_id + 1), date: last_thirty_days_array[date_id - 1])
 			Ratio.create!(due_date_id: (date_id +1), value: rand(0.01..1))
 			Completion.create!(task_id: (task_id + 1), completed: x = completions.sample, completion_value: x = 0 ? rand(1..9) : 10)
 		end
